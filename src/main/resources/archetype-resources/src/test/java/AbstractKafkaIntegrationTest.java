82751ec9d51dcafff0b9e6c0d51dee077b0df32e
crowlandsCH <crowlands@companieshouse.gov.uk>
2023-05-16 17:48:42 +0100

    DSND-1684: Finalise Kafka test containers version. *Test containers version of cp-kafka lines up with 2.0 of apache *When using KafkaContainer cp-kafka auto generate topics is on by default. *Thus, topics are generated in tests as and when needed.

--- src/main/resources/archetype-resources/src/test/java/AbstractKafkaIntegrationTest.java
+++ src/main/resources/archetype-resources/src/test/java/AbstractKafkaIntegrationTest.java
@@ -1,9 +1,5 @@
 package ${package};
 
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -11,30 +7,14 @@ import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @Testcontainers
 @Import(TestKafkaConfig.class)
 public abstract class AbstractKafkaIntegrationTest {
 
     @Container
-    protected static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"));
-
-    @BeforeAll
-    static void configureKafka() {
-        try (AdminClient adminClient = AdminClient.create(
-                Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers()))) {
-            List<NewTopic> topics = List.of(new NewTopic("echo", 1, (short) 1),
-                    new NewTopic("echo-echo-consumer-retry", 1, (short) 1),
-                    new NewTopic("echo-echo-consumer-error", 1, (short) 1),
-                    new NewTopic("echo-echo-consumer-invalid", 1, (short) 1));
-            adminClient.createTopics(topics).all().get(30, TimeUnit.SECONDS);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+    protected static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse(
+            "confluentinc/cp-kafka:5.0.0"));
 
     @DynamicPropertySource
     static void props(DynamicPropertyRegistry registry) {
